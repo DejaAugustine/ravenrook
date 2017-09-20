e108cba388b38bee387b020552cb52dfd73691ce
Déja Augustine <contact@dejaaugustine.com>
2017-09-20 00:44:41 -0500

    Dedupe credits

--- src/components/Credits.js
+++ src/components/Credits.js
@@ -5,7 +5,10 @@ import './Credits.css';
 class Credits extends Component {
   render() {
     const creditList = this.props.credits || [];
-    const filteredList = creditList.filter(function(n){ return n !== undefined && n !== "" });
+    var creditDedupe = {};
+    const filteredList = creditList.filter(function(n){
+      return (n === undefined || n === "" || creditDedupe.hasOwnProperty(n)) ? false  : (creditDedupe[n] = true);
+    });
     const credits = filteredList.join(", ");
 
     return (
